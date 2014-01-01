8d3ec5fbb4c0a0a0aaa7783d5a61f082c37ae8ab
Paul Maynard <paul@ourladyqueenofsaints.org>
2013-12-31 21:03:51 -0500

    Removed everything, starting from scratch

---
+++ js/version.js
@@ -0,0 +1,10 @@
+var VERSION = {
+	major: 0,
+	minor: 1,
+	bugfix: 0,
+	state: 'a',
+	name: 'deuced',
+	toString: function() {
+		return '%s.%s.%s%s \'%s\''.format(this.major, this.minor, this.bugfix, this.state, this.name)
+	}
+};
