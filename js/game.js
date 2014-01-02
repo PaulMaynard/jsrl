d2f2db26729a0f1a739ff38ed9d9612b4c2e76d5
Paul Maynard <paul@ourladyqueenofsaints.org>
2014-01-01 21:47:53 -0500

    first screens

--- js/game.js
+++ js/game.js
@@ -1,2 +1,39 @@
 'use strict';
 document.title = 'jsrl2 v' + VERSION;
+
+function Screen(options, display) {
+	this.display = display || Game.display;
+	if (_.isFunction(options)) {
+		this.render = options;
+	} else {
+		_.assign(this, _.pick(options, ['name', 'enter', 'exit', 'render', 'handleKey']));
+	}
+}
+
+var Game = {
+	width: 80,
+	height: 25,
+	display: null,
+	screens: {},
+	switchScreen: function(name) {
+		if (this.screens.current && this.screens.current.exit) {
+			this.screens.current.exit();
+		}
+		this.screens.current = this.screens[name];
+		if (this.screens.current.enter) {
+			this.screens.current.enter();
+		}
+		this.screens.current.render();
+	}
+};
+Game.display = new ROT.Display({width: Game.width, height: Game.height});
+
+Game.screens.start= new Screen({
+	render: function() {
+		this.display.drawText(5, 5, '%c{red}Hello World!');
+	}
+});
+
+Game.switchScreen('start');
+
+document.body.appendChild(Game.display.getContainer());
--- js/version.js
+++ js/version.js
@@ -1,9 +1,9 @@
 var VERSION = {
 	major: 0,
 	minor: 1,
-	bugfix: 0,
+	bugfix: 1,
 	state: 'a',
-	name: 'deuced',
+	name: 'ra',
 	toString: function() {
 		return '%s.%s.%s%s \'%s\''.format(this.major, this.minor, this.bugfix, this.state, this.name)
 	}
