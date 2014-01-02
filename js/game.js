6104f84e48bd1aaf3c2ef24b993ba033ddde484e
Paul Maynard <paul@ourladyqueenofsaints.org>
2014-01-01 22:41:32 -0500

    Some more test screens

--- js/game.js
+++ js/game.js
@@ -23,14 +23,38 @@ var Game = {
 		if (this.screens.current.enter) {
 			this.screens.current.enter();
 		}
+		this.display.clear();
+		this.screens.current.render();
+	},
+	redraw: function() {
+		this.display.clear();
 		this.screens.current.render();
 	}
 };
 Game.display = new ROT.Display({width: Game.width, height: Game.height});
 
-Game.screens.start= new Screen({
+Game.screens.start = new Screen({
 	render: function() {
 		this.display.drawText(5, 5, '%c{red}Hello World!');
+	}
+});
+Game.screens.main = new Screen(function(){
+	var x = Math.floor(ROT.RNG.getUniform() * Game.width),
+		y = Math.floor(ROT.RNG.getUniform() * Game.height);
+	console.log(x, y);
+	this.display.draw(x, y, '@');
+});
+Game.screens.hello = new Screen(function(){
+	var foreground, background, colors;
+	for (var i = 0; i < 10; i++) {
+	    // Calculate the foreground color, getting progressively darker
+	    // and the background color, getting progressively lighter.
+	    foreground = ROT.Color.interpolate([0, 255, 0], [255, 0, 0], i / 10);
+	    background = ROT.Color.interpolate([255, 255, 255], [0, 0, 0], i / 10);
+	    // Create the color format specifier.
+	    colors = "%c{" + ROT.Color.toRGB(foreground) + "}%b{" + ROT.Color.toRGB(background) + "}";
+	    // Draw the text at col 2 and row i
+	    this.display.drawText(2, i, colors + "Hello, world!");
 	}
 });
 
